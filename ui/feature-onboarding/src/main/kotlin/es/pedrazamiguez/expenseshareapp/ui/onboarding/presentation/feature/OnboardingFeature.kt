416d276a5ea16c2eecdd943e8707134b778350b2
Andrés Pedraza Míguez <pedraza.miguez@gmail.com>
2025-09-22 10:35:51 +0200

    Reused double tap back handler for onboarding

--- ui/feature-onboarding/src/main/kotlin/es/pedrazamiguez/expenseshareapp/ui/onboarding/presentation/feature/OnboardingFeature.kt
+++ ui/feature-onboarding/src/main/kotlin/es/pedrazamiguez/expenseshareapp/ui/onboarding/presentation/feature/OnboardingFeature.kt
@@ -1,26 +1,9 @@
 package es.pedrazamiguez.expenseshareapp.ui.onboarding.presentation.feature
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import es.pedrazamiguez.expenseshareapp.core.ui.extension.placeholder
+import es.pedrazamiguez.expenseshareapp.ui.onboarding.presentation.screen.OnboardingScreen
 
 @Composable
 fun OnboardingFeature(onOnboardingComplete: () -> Unit = {}) {
-
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Button(onClick = {
-            onOnboardingComplete()
-        }) {
-            Text("Complete Onboarding".placeholder)
-        }
-    }
-
+    OnboardingScreen(onOnboardingComplete = onOnboardingComplete)
 }
