27d73bee6b3dc770381ea7cf1e8214f7872892ef
Andrés Pedraza Míguez <pedraza.miguez@gmail.com>
2025-09-23 13:34:29 +0200

    Refactored bottom nav icon animation logic

--- ui/feature-balances/src/main/kotlin/es/pedrazamiguez/expenseshareapp/ui/balance/navigation/impl/BalancesNavigationProviderImpl.kt
+++ ui/feature-balances/src/main/kotlin/es/pedrazamiguez/expenseshareapp/ui/balance/navigation/impl/BalancesNavigationProviderImpl.kt
@@ -1,17 +1,10 @@
 package es.pedrazamiguez.expenseshareapp.ui.balance.navigation.impl
 
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavGraphBuilder
 import compose.icons.TablerIcons
 import compose.icons.tablericons.ChartBar
+import es.pedrazamiguez.expenseshareapp.core.ui.component.NavigationBarIcon
 import es.pedrazamiguez.expenseshareapp.core.ui.extension.placeholder
 import es.pedrazamiguez.expenseshareapp.core.ui.navigation.NavigationProvider
 import es.pedrazamiguez.expenseshareapp.ui.balance.navigation.BALANCES_ROUTE
@@ -25,13 +18,10 @@ class BalancesNavigationProviderImpl(
 
     @Composable
     override fun Icon(isSelected: Boolean) {
-        val scale by animateFloatAsState(targetValue = if (isSelected) 1.2f else 1f)
-
-        Icon(
-            imageVector = TablerIcons.ChartBar,
+        NavigationBarIcon(
+            icon = TablerIcons.ChartBar,
             contentDescription = label,
-            tint = if (isSelected) MaterialTheme.colorScheme.primary else Color.Gray,
-            modifier = Modifier.size((24.dp * scale))
+            isSelected = isSelected
         )
     }
 
--- ui/feature-groups/src/main/kotlin/es/pedrazamiguez/expenseshareapp/ui/group/navigation/impl/GroupsNavigationProviderImpl.kt
+++ ui/feature-groups/src/main/kotlin/es/pedrazamiguez/expenseshareapp/ui/group/navigation/impl/GroupsNavigationProviderImpl.kt
@@ -1,16 +1,10 @@
 package es.pedrazamiguez.expenseshareapp.ui.group.navigation.impl
 
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavGraphBuilder
 import compose.icons.TablerIcons
 import compose.icons.tablericons.Users
+import es.pedrazamiguez.expenseshareapp.core.ui.component.NavigationBarIcon
 import es.pedrazamiguez.expenseshareapp.core.ui.extension.placeholder
 import es.pedrazamiguez.expenseshareapp.core.ui.navigation.NavigationProvider
 import es.pedrazamiguez.expenseshareapp.ui.group.navigation.GROUPS_ROUTE
@@ -22,13 +16,10 @@ class GroupsNavigationProviderImpl : NavigationProvider {
 
     @Composable
     override fun Icon(isSelected: Boolean) {
-        val scale by animateFloatAsState(targetValue = if (isSelected) 1.2f else 1f)
-
-        androidx.compose.material3.Icon(
-            imageVector = TablerIcons.Users,
+        NavigationBarIcon(
+            icon = TablerIcons.Users,
             contentDescription = label,
-            tint = if (isSelected) MaterialTheme.colorScheme.primary else Color.Gray,
-            modifier = Modifier.size((24.dp * scale))
+            isSelected = isSelected
         )
     }
 
