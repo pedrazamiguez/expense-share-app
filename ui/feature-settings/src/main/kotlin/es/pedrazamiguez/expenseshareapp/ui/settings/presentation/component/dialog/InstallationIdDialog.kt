ab697e02d8fbcf105eccbe09c894d2e79581389c
Andrés Pedraza Míguez <pedraza.miguez@gmail.com>
2025-10-05 06:48:06 +0200

    Added copy button on installation ID alert dialog

--- ui/feature-settings/src/main/kotlin/es/pedrazamiguez/expenseshareapp/ui/settings/presentation/component/dialog/InstallationIdDialog.kt
+++ ui/feature-settings/src/main/kotlin/es/pedrazamiguez/expenseshareapp/ui/settings/presentation/component/dialog/InstallationIdDialog.kt
@@ -1,9 +1,14 @@
 package es.pedrazamiguez.expenseshareapp.ui.settings.presentation.component.dialog
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 import es.pedrazamiguez.expenseshareapp.core.ui.extension.placeholder
 
 @Composable
@@ -11,11 +16,33 @@ fun InstallationIdDialog(
     installationId: String,
     onDismiss: () -> Unit
 ) {
+
+    val context = LocalContext.current
+
     AlertDialog(
         onDismissRequest = onDismiss,
         title = { Text("Installation ID".placeholder) },
         text = { Text(installationId) },
         confirmButton = {
+            TextButton(onClick = {
+                // Copy to clipboard
+                val clipboard = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+                clipboard.setPrimaryClip(
+                    ClipData.newPlainText(
+                        "Installation ID",
+                        installationId
+                    )
+                )
+                Toast.makeText(
+                    context,
+                    "Copied to clipboard".placeholder,
+                    Toast.LENGTH_SHORT
+                ).show()
+            }) {
+                Text("Copy".placeholder)
+            }
+        },
+        dismissButton = {
             TextButton(onClick = onDismiss) {
                 Text("Close".placeholder)
             }
