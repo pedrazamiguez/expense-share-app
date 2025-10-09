afe210bfbc5db1c0f3ac5fb5f3ab0893ab2bbdba
Andrés Pedraza Míguez <pedraza.miguez@gmail.com>
2025-10-09 17:30:55 +0200

    Increased version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 1
-        versionName = "0.5.3"
+        versionName = "0.5.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
