300c8375937b93a7c5830d97e1521e7ddaa633df
Andrés Pedraza Míguez <pedraza.miguez@gmail.com>
2025-10-13 06:03:19 +0200

    Increased version and fixed release indicator in workflow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 1
-        versionName = "0.8.6"
+        versionName = "0.9.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
