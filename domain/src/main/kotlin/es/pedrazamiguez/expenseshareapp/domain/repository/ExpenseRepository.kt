257070da731477fe4bff56c96abe3d7ae7aa5347
Andrés Pedraza Míguez <pedraza.miguez@gmail.com>
2025-10-13 07:55:06 +0200

    [EXSHAPP-0192]: User can list expenses for a group (#193)

--- domain/src/main/kotlin/es/pedrazamiguez/expenseshareapp/domain/repository/ExpenseRepository.kt
+++ domain/src/main/kotlin/es/pedrazamiguez/expenseshareapp/domain/repository/ExpenseRepository.kt
@@ -1,10 +1,12 @@
 package es.pedrazamiguez.expenseshareapp.domain.repository
 
 import es.pedrazamiguez.expenseshareapp.domain.model.Expense
+import kotlinx.coroutines.flow.Flow
 
 interface ExpenseRepository {
     suspend fun addExpense(
         groupId: String,
         expense: Expense
     )
+    fun getGroupExpensesFlow(groupId: String): Flow<List<Expense>>
 }
--- ui/feature-expenses/src/main/kotlin/es/pedrazamiguez/expenseshareapp/ui/expense/di/ExpensesUiModule.kt
+++ ui/feature-expenses/src/main/kotlin/es/pedrazamiguez/expenseshareapp/ui/expense/di/ExpensesUiModule.kt
@@ -3,15 +3,18 @@ package es.pedrazamiguez.expenseshareapp.ui.expense.di
 import es.pedrazamiguez.expenseshareapp.core.ui.navigation.NavigationProvider
 import es.pedrazamiguez.expenseshareapp.core.ui.presentation.screen.ScreenUiProvider
 import es.pedrazamiguez.expenseshareapp.domain.usecase.expense.AddExpenseUseCase
+import es.pedrazamiguez.expenseshareapp.domain.usecase.expense.GetGroupExpensesFlowUseCase
 import es.pedrazamiguez.expenseshareapp.ui.expense.navigation.impl.ExpensesNavigationProviderImpl
 import es.pedrazamiguez.expenseshareapp.ui.expense.presentation.screen.impl.AddExpenseScreenUiProviderImpl
 import es.pedrazamiguez.expenseshareapp.ui.expense.presentation.screen.impl.ExpensesScreenUiProviderImpl
 import es.pedrazamiguez.expenseshareapp.ui.expense.presentation.viewmodel.AddExpenseViewModel
+import es.pedrazamiguez.expenseshareapp.ui.expense.presentation.viewmodel.ListGroupExpensesViewModel
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val expensesUiModule = module {
+    viewModel { ListGroupExpensesViewModel(getGroupExpensesFlowUseCase = get<GetGroupExpensesFlowUseCase>()) }
     viewModel { AddExpenseViewModel(addExpenseUseCase = get<AddExpenseUseCase>()) }
 
     factory { ExpensesNavigationProviderImpl() } bind NavigationProvider::class
