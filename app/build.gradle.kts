f6fe7bf4850584bf4f989728704de289213928d5
Andrés Pedraza Míguez <pedraza.miguez@gmail.com>
2025-12-14 10:18:25 +0100

    Tried to fix font issue

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -105,6 +105,7 @@ dependencies {
     implementation(libs.koin.compose)
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.messaging)
+    implementation(libs.play.services.base)
     implementation(libs.coil.compose)
     implementation(libs.timber)
     testImplementation(libs.junit4)
