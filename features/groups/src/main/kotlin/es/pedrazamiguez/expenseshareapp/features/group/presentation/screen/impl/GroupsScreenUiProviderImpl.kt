cab157f299ff20c737481ab7263b6a1434c06536
Andrés Pedraza Míguez <pedraza.miguez@gmail.com>
2025-12-15 21:17:47 +0100

    Applied for groups

--- features/groups/src/main/kotlin/es/pedrazamiguez/expenseshareapp/features/group/presentation/screen/impl/GroupsScreenUiProviderImpl.kt
+++ features/groups/src/main/kotlin/es/pedrazamiguez/expenseshareapp/features/group/presentation/screen/impl/GroupsScreenUiProviderImpl.kt
@@ -7,13 +7,12 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import es.pedrazamiguez.expenseshareapp.core.designsystem.navigation.LocalTabNavController
 import es.pedrazamiguez.expenseshareapp.core.designsystem.navigation.Routes
 import es.pedrazamiguez.expenseshareapp.core.designsystem.presentation.screen.ScreenUiProvider
+import es.pedrazamiguez.expenseshareapp.core.designsystem.presentation.topbar.DynamicTopAppBar
 import es.pedrazamiguez.expenseshareapp.features.group.R
 
 class GroupsScreenUiProviderImpl(
@@ -22,8 +21,9 @@ class GroupsScreenUiProviderImpl(
 
     @OptIn(ExperimentalMaterial3Api::class)
     override val topBar: @Composable () -> Unit = {
-        TopAppBar(
-            title = { Text(stringResource(R.string.groups_title)) },
+        DynamicTopAppBar(
+            title = stringResource(R.string.groups_title),
+            subtitle = stringResource(R.string.groups_subtitle),
             actions = {
                 IconButton(onClick = { }) {
                     Icon(
@@ -31,7 +31,8 @@ class GroupsScreenUiProviderImpl(
                         contentDescription = stringResource(R.string.groups_info)
                     )
                 }
-            })
+            }
+        )
     }
 
     override val fab: @Composable () -> Unit = {
