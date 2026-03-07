908c070658ddbb75d5dda96f5d40ca89f63fec1c
Andrés Pedraza Míguez <pedraza.miguez@gmail.com>
2026-03-07 22:20:01 +0100

    EXSHAPP-0440]: Resolve Concurrency Flaws and Enforce Single-Subscription Rules (#443)

--- data/src/main/kotlin/es/pedrazamiguez/expenseshareapp/data/di/GroupsDataModule.kt
+++ data/src/main/kotlin/es/pedrazamiguez/expenseshareapp/data/di/GroupsDataModule.kt
@@ -1,8 +1,12 @@
 package es.pedrazamiguez.expenseshareapp.data.di
 
 import es.pedrazamiguez.expenseshareapp.data.repository.impl.GroupRepositoryImpl
+import es.pedrazamiguez.expenseshareapp.domain.datasource.cloud.CloudCashWithdrawalDataSource
+import es.pedrazamiguez.expenseshareapp.domain.datasource.cloud.CloudContributionDataSource
 import es.pedrazamiguez.expenseshareapp.domain.datasource.cloud.CloudExpenseDataSource
 import es.pedrazamiguez.expenseshareapp.domain.datasource.cloud.CloudGroupDataSource
+import es.pedrazamiguez.expenseshareapp.domain.datasource.local.LocalCashWithdrawalDataSource
+import es.pedrazamiguez.expenseshareapp.domain.datasource.local.LocalContributionDataSource
 import es.pedrazamiguez.expenseshareapp.domain.datasource.local.LocalExpenseDataSource
 import es.pedrazamiguez.expenseshareapp.domain.datasource.local.LocalGroupDataSource
 import es.pedrazamiguez.expenseshareapp.domain.repository.GroupRepository
@@ -14,7 +18,11 @@ val groupsDataModule = module {
             cloudGroupDataSource = get<CloudGroupDataSource>(),
             localGroupDataSource = get<LocalGroupDataSource>(),
             cloudExpenseDataSource = get<CloudExpenseDataSource>(),
-            localExpenseDataSource = get<LocalExpenseDataSource>()
+            localExpenseDataSource = get<LocalExpenseDataSource>(),
+            cloudContributionDataSource = get<CloudContributionDataSource>(),
+            localContributionDataSource = get<LocalContributionDataSource>(),
+            cloudCashWithdrawalDataSource = get<CloudCashWithdrawalDataSource>(),
+            localCashWithdrawalDataSource = get<LocalCashWithdrawalDataSource>()
         )
     }
 }
