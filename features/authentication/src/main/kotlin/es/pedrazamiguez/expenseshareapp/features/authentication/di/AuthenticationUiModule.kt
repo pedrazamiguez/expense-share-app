629a2dd745ad993b54663d4d59c7d8c069cea72d
Andrés Pedraza Míguez <pedraza.miguez@gmail.com>
2026-03-09 18:53:04 +0100

    Implemented improvement

--- features/authentication/src/main/kotlin/es/pedrazamiguez/expenseshareapp/features/authentication/di/AuthenticationUiModule.kt
+++ features/authentication/src/main/kotlin/es/pedrazamiguez/expenseshareapp/features/authentication/di/AuthenticationUiModule.kt
@@ -1,8 +1,7 @@
 package es.pedrazamiguez.expenseshareapp.features.authentication.di
 
-import es.pedrazamiguez.expenseshareapp.domain.service.AuthenticationService
+import es.pedrazamiguez.expenseshareapp.domain.usecase.auth.SignInWithEmailUseCase
 import es.pedrazamiguez.expenseshareapp.domain.usecase.auth.SignInWithGoogleUseCase
-import es.pedrazamiguez.expenseshareapp.domain.usecase.notification.RegisterDeviceTokenUseCase
 import es.pedrazamiguez.expenseshareapp.features.authentication.presentation.viewmodel.AuthenticationViewModel
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
@@ -10,8 +9,7 @@ import org.koin.dsl.module
 val authenticationUiModule = module {
     viewModel {
         AuthenticationViewModel(
-            authenticationService = get<AuthenticationService>(),
-            registerDeviceTokenUseCase = get<RegisterDeviceTokenUseCase>(),
+            signInWithEmailUseCase = get<SignInWithEmailUseCase>(),
             signInWithGoogleUseCase = get<SignInWithGoogleUseCase>()
         )
     }
