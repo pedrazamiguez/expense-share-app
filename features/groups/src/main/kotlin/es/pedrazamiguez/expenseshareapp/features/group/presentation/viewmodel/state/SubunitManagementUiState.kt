8ee877c7be67f847a13972d406e62e44e232fb1b
Andrés Pedraza Míguez <pedraza.miguez@gmail.com>
2026-03-14 08:30:48 +0100

    Missing file

--- features/groups/src/main/kotlin/es/pedrazamiguez/expenseshareapp/features/group/presentation/viewmodel/state/SubunitManagementUiState.kt
+++ features/groups/src/main/kotlin/es/pedrazamiguez/expenseshareapp/features/group/presentation/viewmodel/state/SubunitManagementUiState.kt
@@ -1,7 +1,5 @@
 package es.pedrazamiguez.expenseshareapp.features.group.presentation.viewmodel.state
 
-import es.pedrazamiguez.expenseshareapp.core.common.presentation.UiText
-import es.pedrazamiguez.expenseshareapp.features.group.presentation.model.SubunitFormState
 import es.pedrazamiguez.expenseshareapp.features.group.presentation.model.SubunitUiModel
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.persistentListOf
@@ -10,10 +8,5 @@ data class SubunitManagementUiState(
     val isLoading: Boolean = true,
     val groupId: String = "",
     val groupName: String = "",
-    val subunits: ImmutableList<SubunitUiModel> = persistentListOf(),
-    val isDialogVisible: Boolean = false,
-    val editingSubunit: SubunitFormState? = null,
-    val nameError: UiText? = null,
-    val membersError: UiText? = null
+    val subunits: ImmutableList<SubunitUiModel> = persistentListOf()
 )
-
