81a1eba849531daa4187d8f6b1897c3c46f7cb0f
Andrés Pedraza Míguez <pedraza.miguez@gmail.com>
2026-04-10 11:43:50 +0200

    [SPLTRP-0897]: Bug: Currency search in "Other currencies" (step 2 of Create Group) doesn't match Spanish names (#906)

--- domain/src/main/kotlin/es/pedrazamiguez/splittrip/domain/enums/Currency.kt
+++ domain/src/main/kotlin/es/pedrazamiguez/splittrip/domain/enums/Currency.kt
@@ -65,6 +65,126 @@ enum class Currency(val fullName: String, val symbol: String) {
     NZD(
         "New Zealand Dollar",
         "NZ$"
+    ),
+
+    // Thai Baht
+    THB(
+        "Thai Baht",
+        "฿"
+    ),
+
+    // South Korean Won
+    KRW(
+        "South Korean Won",
+        "₩"
+    ),
+
+    // Indian Rupee
+    INR(
+        "Indian Rupee",
+        "₹"
+    ),
+
+    // Brazilian Real
+    BRL(
+        "Brazilian Real",
+        "R$"
+    ),
+
+    // Argentine Peso
+    ARS(
+        "Argentine Peso",
+        "$"
+    ),
+
+    // Colombian Peso
+    COP(
+        "Colombian Peso",
+        "$"
+    ),
+
+    // Chilean Peso
+    CLP(
+        "Chilean Peso",
+        "$"
+    ),
+
+    // Peruvian Sol
+    PEN(
+        "Peruvian Sol",
+        "S/"
+    ),
+
+    // Danish Krone
+    DKK(
+        "Danish Krone",
+        "kr"
+    ),
+
+    // Norwegian Krone
+    NOK(
+        "Norwegian Krone",
+        "kr"
+    ),
+
+    // Polish Zloty
+    PLN(
+        "Polish Zloty",
+        "zł"
+    ),
+
+    // Czech Koruna
+    CZK(
+        "Czech Koruna",
+        "Kč"
+    ),
+
+    // Hungarian Forint
+    HUF(
+        "Hungarian Forint",
+        "Ft"
+    ),
+
+    // Turkish Lira
+    TRY(
+        "Turkish Lira",
+        "₺"
+    ),
+
+    // Moroccan Dirham
+    MAD(
+        "Moroccan Dirham",
+        "MAD"
+    ),
+
+    // UAE Dirham
+    AED(
+        "UAE Dirham",
+        "د.إ"
+    ),
+
+    // South African Rand
+    ZAR(
+        "South African Rand",
+        "R"
+    ),
+
+    // Singapore Dollar
+    SGD(
+        "Singapore Dollar",
+        "S$"
+    ),
+
+    // Hong Kong Dollar
+    HKD(
+        "Hong Kong Dollar",
+        "HK$"
+    ),
+
+    // Indonesian Rupiah
+    IDR(
+        "Indonesian Rupiah",
+        "Rp"
     );
 
     companion object {
